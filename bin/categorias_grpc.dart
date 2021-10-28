a05cef6205a66a40b996882b5683570636db74c4
Albinson Arias <arias.almonte@gmail.com>
2021-10-28 09:51:35 -0400

    Initial Commit

---
+++ bin/categorias_grpc.dart
@@ -0,0 +1,5 @@
+import 'package:categorias_grpc/categorias_grpc.dart' as categorias_grpc;
+
+void main(List<String> arguments) {
+  print('Hello world: ${categorias_grpc.calculate()}!');
+}
---
+++ test/categorias_grpc_test.dart
@@ -0,0 +1,8 @@
+import 'package:categorias_grpc/categorias_grpc.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
