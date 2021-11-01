69466771a56b2af9752678551a6ef6eec8670c94
Albinson Arias <arias.almonte@gmail.com>
2021-11-01 12:51:21 -0400

    Arreglar where

--- lib/bin/client.dart
+++ lib/bin/client.dart
@@ -1,13 +1,15 @@
+import 'package:categorias_grpc/src/model/gen/ship/dart/google/protobuf/struct.pb.dart';
 import 'package:categorias_grpc/src/model/gen/ship/dart/lib/src/model/protodefs/categories.pbgrpc.dart';
 import 'package:categorias_grpc/src/model/gen/ship/dart/lib/src/model/protodefs/filter.pb.dart';
 import 'package:categorias_grpc/src/model/gen/ship/dart/lib/src/model/protodefs/productos.pbgrpc.dart';
 import 'package:grpc/grpc.dart';
 
 void main(List<String> args) async {
+  print('A');
   final channel = ClientChannel(
     // '18.119.2.47',
     // port: 7985,
-    'localhost',
+    '192.168.0.6',
     port: 50051,
     options: ChannelOptions(
       credentials: ChannelCredentials.insecure(),
@@ -18,17 +20,16 @@ void main(List<String> args) async {
   final stub = ProductosSrvClient(channel);
 
   List<WhereClause> where = [];
+  where.add(WhereClause()
+    ..columnName = "prod_nombre"
+    ..conector = WhereConector.none
+    ..value1 = Value(stringValue: 'P')
+    ..operator = WhereOperator.contains);
 
-  where.add(WhereClause(
-      columnName: 'nombre',
-      operator: WhereOperator.equals,
-      selectedValue: 'Abrigos_Y_Jackets',
-      conector: WhereConector.none));
-
-  final resultado =
-      await stub.consultarProducto(FilterRequest(rowsOffKeyset: 0));
+  final resultado = await stub
+      .consultarProducto(FilterRequest(rowsOffKeyset: 0, rowsPerPage: 10));
 
   print('Llego aqui');
-  print(resultado);
+  print(resultado.data);
   // print(resultado);
 }
